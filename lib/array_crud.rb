579a03ca8a4c84a069a358c4dec049176270cff7
Patrick Richard <prichard191@gmail.com>
2019-11-02 20:27:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  new_array[4] << "zoop"
+  new_array<< "zoop"
 
 end
 
@@ -42,3 +42,21 @@ end
 def update_element_from_index(array, index_number, element)
   new_array[1]= "joop"
 end
+
+
+add_element_to_end_of_array
+
+add_element_to_start_of_array
+
+remove_element_from_end_of_array
+
+remove_element_from_start_of_array
+
+retrieve_last_element_from_array
+
+retrieve_element_from_index
+
+retrieve_first_element_from_array
+
+update_element_from_index
+
